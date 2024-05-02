e7c2ab70823c65ee2839e3f3d05a971d61acac65
Eric Brianez Giannetti <63002508+Eric-Giannetti@users.noreply.github.com>
2024-05-02 09:38:15 -0300

    gitgnore

--- InterfacesProject/obj/Debug/net8.0/InterfacesProject.AssemblyInfo.cs
+++ InterfacesProject/obj/Debug/net8.0/InterfacesProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InterfacesProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7586fbdda2083549a699bfa356b084053213956e")]
 [assembly: System.Reflection.AssemblyProductAttribute("InterfacesProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InterfacesProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
